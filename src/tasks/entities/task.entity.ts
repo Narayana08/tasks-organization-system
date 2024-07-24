1e6a6e292ad563ea548a86d230219273749867e5
Narayana08 <narayana.c@techouts.com>
2024-07-25 02:49:55 +0530

    implemented task associated for each user with authentication

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,27 @@
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
+@Entity()
+export class Task {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+
+  @ManyToOne(() => User, user => user.tasks, { eager: false })
+  user: User;
+}
---
+++ src/tasks/entities/task.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
+import { Repository } from 'typeorm';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { Task, TaskStatus } from './task.entity';
+
+
+@Injectable()
+export class TaskService {
+
+  @InjectRepository(Task) private taskRepository: Repository<Task>;
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    task.user = user;
+    await this.taskRepository.save(task);
+
+    return task;
+  }
+
+  async deleteTask(id: number, user: User): Promise<any> {
+    const result = await this.taskRepository.delete({ id });
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+    return result
+  }
+}
